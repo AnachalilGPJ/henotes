594aff132ebc6dcdc8905ec471113a5e2e54790d
George Johns <george.johns@griffithuni.edu.au>
2026-05-14 05:56:22 +1000

    Refactor Page component with updated class names

--- src/ui/page.tsx
+++ src/ui/page.tsx
@@ -4,7 +4,9 @@ import Verse from "./Verse";
 
 export default function Page({ verses }) {
   return (
-    <div className="h-full bg-[#fbf8f2] p-6 flex flex-col gap-4 font-serif text-[15px] leading-relaxed">
+    
+<div className="page p-6 h-full flex flex-col gap-4">
+
 
       {verses.map(v => (
         <Verse key={v.id} verse={v} />
