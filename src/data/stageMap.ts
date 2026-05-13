93afcd6274a365ad08dc7fec59e38503681c1985
George Johns <george.johns@griffithuni.edu.au>
2026-05-14 04:49:25 +1000

    Create stageMap.ts with BOOK_STAGE_MAP
    Added a mapping of biblical books to their corresponding stages.

---
+++ src/data/stageMap.ts
@@ -0,0 +1,38 @@
+// src/data/
+
+export const BOOK_STAGE_MAP = {
+  Genesis: "creation",
+  Exodus: "liberation",
+  Leviticus: "law",
+  Numbers: "testing",
+  Deuteronomy: "law",
+
+  Joshua: "settlement",
+  Judges: "failure",
+
+  "1 Samuel": "kingship_rise",
+  "2 Samuel": "kingship",
+  "1 Kings": "kingship_peak",
+  "2 Kings": "collapse",
+
+  Psalms: "participation",
+  Proverbs: "wisdom",
+  Ecclesiastes: "wisdom",
+
+  Isaiah: "prophetic",
+  Jeremiah: "prophetic",
+  Ezekiel: "prophetic",
+
+  Daniel: "expectation",
+
+  Matthew: "incarnation",
+  Mark: "incarnation",
+  Luke: "incarnation",
+  John: "incarnation",
+
+  Acts: "participation",
+  Romans: "fulfillment",
+  Hebrews: "fulfillment",
+
+  Revelation: "consummation"
+};
