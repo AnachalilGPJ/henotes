aa8dbe2a79a281ce9f75b09b5c231ce091f17fdc
George Johns <george.johns@griffithuni.edu.au>
2026-05-14 05:38:32 +1000

    Create page.tsx

---
+++ src/ui/page.tsx
@@ -0,0 +1,16 @@
+// src/ui/Page.tsx
+
+import Verse from "./Verse";
+
+export default function Page({ verses }) {
+  return (
+    <div className="h-full bg-[#fbf8f2] p-6 flex flex-col gap-4 font-serif text-[15px] leading-relaxed">
+
+      {verses.map(v => (
+        <Verse key={v.id} verse={v} />
+      ))}
+
+    </div>
+  );
+}
+``
